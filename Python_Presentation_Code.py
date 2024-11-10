012251e20da90b4bba11a81ae00e0985f64f2384
Kobby Jones <kobbyj099@gmail.com>
2024-11-10 14:51:47 +0000

    I created a python_practice fiile

---
+++ Python_Presentation_Code.py
@@ -0,0 +1,64 @@
+
+# 1. Numeric Data Types
+# Integers, floats, and complex numbers in Python
+int_num = 10   
+float_num = 3.14 
+complex_num = 2 + 3j 
+# print(int_num, float_num, complex_num)
+
+# 2. String Data Type
+# Strings in Python can be created using single or double quotes
+string_example = "Hello, Python!" 
+multi_line_string = '''This is a
+multi-line string example'''
+# print(string_example)
+# print(multi_line_string)
+
+# 3. List, Tuple, Dictionary
+# Examples of list, tuple, and dictionary in Python
+sample_list = [1, 2, 3, 4]          # List - mutable
+sample_tuple = (1, 2, 3, 4)         # Tuple - immutable
+sample_dict = {"name": "Alice", "age": 25}  # Dictionary - key-value pairs
+# print("List:", sample_list)
+# print("Tuple:", sample_tuple)
+# print("Dictionary:", sample_dict)
+
+# 4. Categorize Values
+# Checking types of values in Python
+value1 = 42
+value2 = "Hello"
+value3 = 3.14
+# print("Type of value1:", type(value1))
+# print("Type of value2:", type(value2))
+# print("Type of value3:", type(value3))
+
+# 5. Composite Data Types
+# Composite data types can hold multiple items
+nested_list = [1, [2, 3], [4, 5]]  # List containing lists
+nested_dict = {"numbers": [1, 2, 3], "letters": ["a", "b", "c"]}  # Dict containing lists
+# print("Nested List:", nested_list)
+# print("Nested Dictionary:", nested_dict)
+
+# 6. Conditionals
+# Using if, elif, and else for decision making
+num = 10
+# if num > 10:
+#     print("The number is greater than 10")
+# elif num == 10:
+#     print("The number is equal to 10")
+# else:
+#     print("The number is less than 10")
+
+# 7. Loops
+# Using for and while loops in Python
+
+# For loop example
+numbers = [1, 2, 3, 4, 5]
+# for num in numbers:
+#     print("For loop number:", num)
+
+# While loop example
+count = 0
+# while count < 5:
+#     print("While loop count:", count)
+#     count += 1
---
+++ practice.py
@@ -0,0 +1,57 @@
+num_integer = 20
+print(num_integer)
+num_float = 4.23
+print(num_float)
+
+num_complex = 5j
+print(num_complex)
+
+string_hello = "Hello welcome"
+print(string_hello)
+another_string = 'Hey there'
+print(another_string)
+triple_string = '''Hello Everyone! I'm learning python'''
+print(triple_string)
+
+list = [1, 2, 3, 5]
+tuples = (1, 3, 6)
+dictionary = {"Name": "Cobbina", "Age": 24}
+print(list)
+print(tuples)
+print(dictionary)
+list.append(10)
+print(list)
+list.insert(2, 20)
+print(list)
+
+print(tuples[1])
+print(dictionary['Name'])
+
+print(type(num_integer))
+print(type(num_float))
+print(type(num_complex))
+
+print(type(list))
+print(type(tuples))
+print(type(dictionary))
+
+
+list2 = [1, 2, [3,5,6], 3, 7, [9]]
+print(list2[2][2])
+
+age = 75
+if(age > 40  and age <60):
+    print("You are an adult")
+elif(age <= 60):
+    print("You are getting older")
+else:
+    print("You are a senior citizen")
+
+for i in range(10):
+    print(i)
+
+
+counter = 0
+while(counter < 10):
+    print(counter)
+    counter = counter+1
